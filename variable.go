a320c7669627cbe0bbec87d4f5f8cc73c99d2224
Yusuf Supriadi <yusufdolenk2@gmail.com>
2025-02-22 15:55:49 +0700

    feat: add variable.go

---
+++ variable.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	name = "Yusuf"
+	fmt.Println(name)
+
+	name = "Supriadi"
+	fmt.Println(name)
+
+	name = "Yusuf Supriadi"
+	fmt.Println(name)
+}
