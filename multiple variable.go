40c72a3bdfea97a1d1dcb8caf3f22497f6eb1f18
Yusuf Supriadi <yusufdolenk2@gmail.com>
2025-02-22 16:03:29 +0700

    feat: add ultiple variable

---
+++ multiple variable.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		firstName = "Yusuf"
+		lastName  = "Supriadi"
+		fullName  = "Yusuf Supriadi"
+	)
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(fullName)
+}
