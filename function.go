bae9cb58a412a4810f64f4fd63857f971280063a
Yusuf Supriadi <yusufdolenk2@gmail.com>
2025-02-22 19:21:03 +0700

    feat: add function.go

---
+++ function.go
@@ -0,0 +1,14 @@
+package main
+
+func sayHello() {
+	println("Hello Yusuf Supriadi")
+}
+
+func sayHelloTo(firstName string, lastName string) {
+	println("Hello", firstName, lastName)
+}
+
+func main() {
+	sayHello()
+	sayHelloTo("Firlana", "Luchiana Dewi")
+}
