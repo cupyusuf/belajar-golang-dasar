7b21276df92fa2f1e4fd06992ac24a14033df50f
Yusuf Supriadi <yusufdolenk2@gmail.com>
2025-02-23 08:45:27 +0700

    feat: add access modifier

--- helper/helper.go
+++ helper/helper.go
@@ -1,5 +1,13 @@
 package helper
 
+var version = "1.0.0" // private (tidak bisa diakses diluar package)
+var Application = "Golang"
+
+// Tidak bisa diakses diluar package
+func sayGoodBye(name string) string {
+	return "Good Bye " + name
+}
+
 func SayHello(name string) string {
 	return "Hello " + name
 }
--- import.go
+++ import.go
@@ -8,4 +8,8 @@ import (
 func main() {
 	result := helper.SayHello("Yusuf")
 	fmt.Println(result)
+
+	fmt.Println(helper.Application)
+	// fmt.Println(helper.Version) //  private
+	// fmt.Println(helper.sayGoodBye("Yusuf")) // private
 }
